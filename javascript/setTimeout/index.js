610d274019c14bd4ed529c07891b53e23f3c1afb
Zachary Polof <46513785+IVIonsters@users.noreply.github.com>
2024-12-08 22:19:51 -0600

    Add cookie consent modal and new setTimeout example

---
+++ javascript/setTimeout/index.js
@@ -0,0 +1,9 @@
+console.log('what is the capital of France?');
+
+setTimeout(() => {
+  console.log('Paris');
+}, 3000);
+
+setTimeout(() => {
+  console.log('Ready for the next question?');
+}, 6000);
