19ddd1535e8b6e5d2e451e5a57164fe8af32ab86
Zachary Polof <46513785+IVIonsters@users.noreply.github.com>
2024-12-12 21:38:41 -0600

    Add loading animation and update modal text on form submission

--- javascript/cookie-consent-intro/index.js
+++ javascript/cookie-consent-intro/index.js
@@ -1,6 +1,8 @@
 const modal = document.getElementById('modal');
 const modalClose = document.getElementById('modal-close-btn');
+const modalText = document.getElementById('modal-text');
 
+// Set a timeout to show the modal after 1.5 seconds
 setTimeout(() => {
   modal.style.display = 'block';
 }, 1500);
@@ -9,9 +11,18 @@ modalClose.addEventListener('click', () => {
   modal.style.display = 'none';
 });
 
-const formId = document.getElementById('consent-form')
+const formId = document.getElementById('consent-form');
 
+// Prevent the form from submitting and replace modal text
 formId.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log('Form submitted');
+  modalText.innerHTML = `
+    <div class="modal-inner-loading">
+      <img src="images/loading.svg" class="loading">
+      <p id="uploadText">
+        Uploading your data to the dark web...
+      </p>
+    </div>
+  `;
 });
+
